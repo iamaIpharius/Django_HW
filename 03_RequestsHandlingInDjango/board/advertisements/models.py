42749d8e47001c69dd952f50215e8a5af2a71eed
Alex <ssturmmann@yandex.ru>
2022-06-20 16:19:35 +0700

    homework 5.3, 5.4

--- 03_RequestsHandlingInDjango/board/advertisements/models.py
+++ 03_RequestsHandlingInDjango/board/advertisements/models.py
@@ -8,5 +8,21 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     price = models.FloatField(verbose_name='цена', default=0)
-    views_count = models.IntegerField(verbose_name='количество промотров', default=0)
+    views_count = models.IntegerField(
+        verbose_name='количество промотров', default=0)
+    status = models.ForeignKey(
+        'AdvertisementStatus', default=None, null=True, on_delete=models.CASCADE, related_name='advertisements')
+    type_of_ad = models.ForeignKey(
+        'AdvertisementType', default=None, null=True, on_delete=models.CASCADE, related_name='advertisements'
+    )
+    class Meta:
+        db_table = 'advertisements'
+        ordering = ['title']
 
+
+class AdvertisementStatus(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class AdvertisementType(models.Model):
+    type_name = models.CharField(max_length=100)
