80442736005253d6f77c392515063a4c4928b749
Alex <ssturmmann@yandex.ru>
2022-07-05 15:04:25 +0700

    adminka done with news

--- 05_Forms/news/app_news/admin.py
+++ 05_Forms/news/app_news/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
 from app_news.models import News, Commentary
 
-# Register your models here.
-@admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
-    pass
 
-@admin.register(Commentary)
+class CommentaryInLine(admin.TabularInline):
+    model = Commentary
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'active']
+    list_filter = ['active']
+    inlines = [CommentaryInLine]
+
+    actions = ['set_active', 'set_inactive']
+
+    def set_active(self, request, queryset):
+        queryset.update(active=True)
+
+    def set_inactive(self, request, queryset):
+        queryset.update(active=False)
+
+    set_active.short_description = 'Make Active'
+    set_inactive.short_description = 'Make Inactive'
+
 class CommentaryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 
+                    lambda x: x.text if len(x.text) < 15 else x.text[:15] + '...',
+                    'created_at', 'related_news']
+
+    list_filter = ['name']
+
+    actions = ['delete_comment']
+
+    def delete_comment(self, request, queryset):
+        queryset.update(text='Удалено администратором')
+
+    delete_comment.short_description = 'Удалить комментарий'
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Commentary, CommentaryAdmin)
--- 05_Forms/news/app_news/models.py
+++ 05_Forms/news/app_news/models.py
@@ -2,24 +2,32 @@ from django.db import models
 
 
 class News(models.Model):
+    STATUS_CHOICES = [
+        (True, 'Active'),
+        (False, 'Inactive')
+    ]
     title = models.CharField(max_length=1500, verbose_name='Название')
     content = models.TextField(verbose_name='Содержание')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    active = models.BooleanField()
+    active = models.BooleanField(choices=STATUS_CHOICES, default=False)
 
     class Meta:
         ordering = ['created_at']
 
     def __str__(self):
-        return self.title
+        return f'{self.title}, {self.created_at}, {self.active}'
 
 
 class Commentary(models.Model):
+    DELETE_CHOICE = [('delete', 'Удалено администратором')]
     name = models.CharField(max_length=100, verbose_name='Имя')
-    text = models.TextField(max_length=1000, verbose_name='Текст комментария')
+    text = models.TextField(max_length=1000, verbose_name='Текст комментария', choices=DELETE_CHOICE)
     created_at = models.DateTimeField(auto_now_add=True)
     related_news = models.ForeignKey('News', default=None, null=True, on_delete=models.CASCADE,
                                      verbose_name='Новость')
     class Meta:
         ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.name}, {self.created_at}, {self.text[:15]}'
